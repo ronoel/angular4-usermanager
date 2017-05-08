5d24a91b647401be71bbff7ce00e49447be288f0
Ronoel <ronoeljr@gmail.com>
2017-05-08 17:55:28 -0300

    - Changed keyup event to request users

--- src/client/app/busca/busca.component.ts
+++ src/client/app/busca/busca.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMapTo';
+
 import { UserService } from '../services/user.service';
 
 import { UserEntity } from '../services/userEntity';
@@ -18,7 +23,7 @@ export class BuscaComponent {
     public itemList: UserEntity[];
 
     public filter: string;
-    private _filterSubscription: Subscription;
+    // private _filterSubscription: Subscription;
 
     constructor(
         private _userService: UserService
@@ -29,6 +34,19 @@ export class BuscaComponent {
 
         this._userService.getAll(this.filter).subscribe(data => this.itemList = data);
 
+        let elem : any = document.getElementById('idFilter');
+
+        Observable.fromEvent(elem,'keyup')
+            .map(event => event.target.value )
+            .debounceTime(500)
+            // .filter(text => text.length >= 3)
+            .distinctUntilChanged()
+            // .switchMapTo( value => this._userService.getAll(value))
+            // .subscribe( data => this.itemList = data,
+            //     () => console.log('ERROR'),
+            //     () => console.log('COMPLETE')
+            // );
+            .subscribe( value => this._userService.getAll(value).subscribe(data => this.itemList = data) );
 
     }
 
@@ -43,21 +61,25 @@ export class BuscaComponent {
         );
     }
 
+
+
     public search() {
 
-        if (this._filterSubscription) {
-            console.log('removeu: ')
-            this._filterSubscription.unsubscribe();
-        }
+        // replaced for Observable.fromEvent
 
-        let obs = new Observable(
-            (observer) => {
-                setTimeout(() => {
-                    observer.next();
-                }, 500);
-            }
-        ).switchMap(()=>this._userService.getAll(this.filter));
-        this._filterSubscription = obs.subscribe(data => this.itemList = data);
+    //     if (this._filterSubscription) {
+    //         console.log('removeu: ')
+    //         this._filterSubscription.unsubscribe();
+    //     }
+
+    //     let obs = new Observable(
+    //         (observer) => {
+    //             setTimeout(() => {
+    //                 observer.next();
+    //             }, 500);
+    //         }
+    //     ).switchMap(()=>this._userService.getAll(this.filter));
+    //     this._filterSubscription = obs.subscribe(data => this.itemList = data);
     }
 
 }
